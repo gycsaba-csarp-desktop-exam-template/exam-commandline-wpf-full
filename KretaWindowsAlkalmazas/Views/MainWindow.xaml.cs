d028b71ac167330217a37481474cf2051fe406df
Csaba Gyuris <gyuris.csaba@vasvari.org>
2022-04-07 11:31:43 +0200

    Üdvözlő szöveg megjelenik

--- KretaWindowsAlkalmazas/Views/MainWindow.xaml.cs
+++ KretaWindowsAlkalmazas/Views/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Kreta.View
+namespace Kreta.Views
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
