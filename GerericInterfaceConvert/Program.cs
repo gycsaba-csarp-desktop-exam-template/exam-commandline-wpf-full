5f32ec2d53ddf8eadb87bc67626ecf33f9f4ed04
Csaba Gyuris <gyuris.csaba@vasvari.org>
2022-05-26 14:48:51 +0200

    A példa

---
+++ GerericInterfaceConvert/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace GerericInterfaceConvert
+{
+    public interface BaseInterface { }
+    public interface ChildInterface : BaseInterface { }
+
+    public class ConcreteClass : AbstractClass<ChildInterface> { }
+
+
+    
+    public interface IAbstractClass<out T> where T : BaseInterface { }
+    public abstract class AbstractClass<T> : IAbstractClass<T> where T : BaseInterface { }
+
+
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IAbstractClass<BaseInterface> c = new ConcreteClass();
+        }
+    }
+}
