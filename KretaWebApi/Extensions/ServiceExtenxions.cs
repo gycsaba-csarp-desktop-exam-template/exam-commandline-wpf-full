d4f9bc52b297202f8c5683fe7ec8368d20aab39f
gycsaba <gyuris.csaba@vasvari.org>
2022-07-20 17:48:11 +0200

    NLog beépítése

--- KretaWebApi/Extensions/ServiceExtenxions.cs
+++ KretaWebApi/Extensions/ServiceExtenxions.cs
@@ -1,11 +1,16 @@
-﻿namespace KretaWebApi.Extensions
+﻿using KretaWebApiContracts;
+using KretaWEbApiLoggerService;
+
+namespace KretaWebApi.Extensions
 {
     public static class ServiceExtenxions
     {
         public static void ConfigureCors(this IServiceCollection services)
         {
+            // Cors-> külső erőforrások elérése
             services.AddCors(options =>
             {
+                // Bármelyik címről, bármelyik metódus (Post,Get) és bármilyen fejlécel jön a HTTP kérés
                 options.AddPolicy("CorsPolicy",
                     builder => builder.AllowAnyOrigin()
                     .AllowAnyMethod()
@@ -14,12 +19,18 @@
             });
         }
 
+        // A windows webserver configuráció alapértelmezett
         public static void ConfigureIISIntegration(this IServiceCollection services)
         {
             services.Configure<IISOptions>(options =>
             {
 
             });
+        }
+
+        public static void ConfigureLoggerService(this IServiceCollection services)
+        {
+            services.AddSingleton<ILoggerManager, LoggerManager>();
         }
     }
 }
--- KretaWebApi/Program.cs
+++ KretaWebApi/Program.cs
@@ -1,7 +1,20 @@
+using KretaWebApi.Extensions;
+using Microsoft.AspNetCore.HttpOverrides;
+using NLog;
+
 var builder = WebApplication.CreateBuilder(args);
+
+LogManager.LoadConfiguration(string.Concat(Directory.GetCurrentDirectory(), "/nlog.config"));
+
+// Külsõ erõforrások elérése
+builder.Services.ConfigureCors();
+// WEbszerver konfiguráció
+builder.Services.ConfigureIISIntegration();
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+
+builder.Services.ConfigureLoggerService();
 
 var app = builder.Build();
 
@@ -13,7 +26,18 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
+
+// proxy fejlés továbbítása az aktuális kérésnél, a Linux használata esetén nyújt segítséget
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+    ForwardedHeaders = ForwardedHeaders.All
+});
+
+app.UseCors("CorsPolicy");
+
+
 app.UseHttpsRedirection();
+// a kéréshez statikus fájlok használatát teszi lehetõvé, ha nem adjuk meg az útvonalat, akkor a wwwroot-lesz
 app.UseStaticFiles();
 
 app.UseRouting();
