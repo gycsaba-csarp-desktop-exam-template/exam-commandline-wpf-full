2b3b2d83466f919329c1e7cef0b618755d499c9c
gycsaba <gyuris.csaba@vasvari.org>
2022-07-20 15:57:24 +0200

    ServiceExtenxions

--- KretaWebApi/Extensions/ServiceExtenxions.cs
+++ KretaWebApi/Extensions/ServiceExtenxions.cs
@@ -1,6 +1,25 @@
 ﻿namespace KretaWebApi.Extensions
 {
-    public class ServiceExtenxions
+    public static class ServiceExtenxions
     {
+        public static void ConfigureCors(this IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy",
+                    builder => builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+                    );
+            });
+        }
+
+        public static void ConfigureIISIntegration(this IServiceCollection services)
+        {
+            services.Configure<IISOptions>(options =>
+            {
+
+            });
+        }
     }
 }
