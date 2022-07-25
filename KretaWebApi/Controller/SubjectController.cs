6ee7fe5e296620395a286b56894681a9764d8bc8
gycsaba <gyuris.csaba@vasvari.org>
2022-07-25 15:59:01 +0200

    SubjectController

---
+++ KretaWebApi/Controller/SubjectController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+
+/*
+Kétféle útvonalválasztás létezik:
+
+Konvenció alapú útválasztás és
+Attribútum alapú útválasztás
+
+A konvenció alapú útválasztást azért nevezik így, mert egy konvenciót hoz létre az URL-útvonalakra vonatkozóan.  Az első rész a vezérlő nevének, a második rész a műveleti módszernek, a harmadik rész pedig az opcionális paramétereknek a leképezését végzi. 
+
+Az attribútumos útválasztás az attribútumokat használja, hogy az útvonalakat közvetlenül a vezérlőn belüli műveleti metódusokra képezze le. Általában az alap útvonalat a vezérlő osztály fölé helyezzük, ahogyan azt a Web API vezérlő osztályunkban is láthatjuk. Hasonlóképpen, az egyes műveleti metódusokhoz közvetlenül fölöttük hozzuk létre az útvonalaikat.
+ */
+
+namespace KretaWebApi.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SubjectController : ControllerBase
+    {
+    }
+}
