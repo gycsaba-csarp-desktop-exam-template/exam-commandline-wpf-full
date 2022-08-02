8bc6e0ef6303abe494fd1b0ad2c03a0f0035812b
gycsaba <gyuris.csaba@vasvari.org>
2022-08-02 16:10:37 +0200

    Razor projekt létrejött

---
+++ KretaRazorPages/Pages/Index.cshtml.cs
@@ -0,0 +1,21 @@
+﻿using Kreta.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace KretaRazorPages.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public RepositoryWrapper repository;
+        
+        public IndexModel(RepositoryWrapper repository)
+        {
+            this.repository = repository;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
---
+++ KretaRazorPages/Program.cs
@@ -0,0 +1,29 @@
+using KretaWebApi.Extensions;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+
+// Mysql server elérés konfigurálás
+builder.Services.ConfigureMySqlContext(builder.Configuration);
+
+// RepositoryWrapper
+builder.Services.ConfigureWrapperRepository();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
