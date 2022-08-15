fc2ee7229e5d056138c45d78a82d631aaf5ab299
gycsaba <gyuris.csaba@vasvari.org>
2022-08-15 17:26:13 +0200

    Új tantárgy form

---
+++ KretaRazorPages/Pages/Menu/Subject/Create.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace KretaRazorPages.Pages.Menu.Subject
+{
+    public class CreateModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
