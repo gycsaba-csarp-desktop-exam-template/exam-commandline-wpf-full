b16b7ed32c015bd243af13e3fb021803fd6759f8
Csaba Gyuris <gyuris.csaba@vasvari.org>
2022-08-29 14:55:03 +0200

    ListSubjectView elkezdve

--- KretaDesktop/ViewModel/HeaderWithContentListViewModel.cs
+++ KretaDesktop/ViewModel/HeaderWithContentListViewModel.cs
@@ -9,6 +9,36 @@ namespace KretaDesktop.ViewModel
 {
     public class HeaderWithContentListViewModel : ViewModelBase
     {
+        private RelayCommand UpdateContentViewCommand;
+        private ViewModelBase selectedView;
 
+        public ViewModelBase SelectedView
+        {
+            get
+            {
+                return selectedView;
+            }
+            set
+            {
+                selectedView = value;
+                OnPropertyChanged(nameof(SelectedView));
+            }
+        }
+
+        public HeaderWithContentListViewModel()
+        {
+            UpdateContentViewCommand = new RelayCommand(parameter => UpdateContentView(parameter));
+        }
+
+        public void UpdateContentView(object parameter)
+        {
+            if (parameter is string)
+            {
+                if (parameter.ToString()== "Tantargyak")
+                {
+                    SelectedView = new ContentListSubjectViewModel();
+                }
+            }
+        }
     }
 }
