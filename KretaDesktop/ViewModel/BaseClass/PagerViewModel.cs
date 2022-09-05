daf8cbb390c58478566323c0ec13faecfaf40039
Csaba Gyuris <gyuris.csaba@vasvari.org>
2022-09-05 08:31:57 +0200

    Lapozás működik

--- KretaDesktop/ViewModel/BaseClass/PagerViewModel.cs
+++ KretaDesktop/ViewModel/BaseClass/PagerViewModel.cs
@@ -14,32 +14,44 @@ namespace KretaDesktop.ViewModel.BaseClass
 {
     public abstract class PagedViewModel : PageParameterViewModelBase
     {
-        private ObservableCollection<string> rowPerPage;
+        private ObservableCollection<string> rowPerPagePossibilities;
 
         public ObservableCollection<string> RowPerPagePossibilities
         {
-            get { return rowPerPage; }
+            get { return rowPerPagePossibilities; }
             set 
             { 
-                rowPerPage = value;
+                rowPerPagePossibilities = value;
                 OnPropertyChanged(nameof(RowPerPagePossibilities));
             }
         }
 
+        public int CurrentPage
+        {
+            get { return QueryString.CurrentPage; }
+        }
+
         public RelayCommand LastPageCommand {get; set;}
+        public RelayCommand FirstPageCommand { get; set;}
+        public RelayCommand PreviusPageCommand { get; set;}
+        public RelayCommand NextPageCommand { get; set;}
 
         public PagedViewModel()
         {
             LastPageCommand = new RelayCommand(execute => LastPage());
+            FirstPageCommand = new RelayCommand(execute => FirstPage());
+            PreviusPageCommand = new RelayCommand(execute => PreviusPage());
+            NextPageCommand = new RelayCommand(execute => NextPage());
+            
             AppConfiguration appConfiguration = new AppConfiguration();
-            rowPerPage=new ObservableCollection<string>(appConfiguration.GetPossibleNumberOfRowOnTheDataGridTable());
+            rowPerPagePossibilities=new ObservableCollection<string>(appConfiguration.GetPossibleNumberOfRowOnTheDataGridTable());
 
             QueryString = new QueryStringParameters();
             QueryString.CurrentPage = 1;
             QueryString.OrderBy = String.Empty;
             QueryString.Fields = String.Empty;
-            if (rowPerPage.Count > 0)
-                SetPageSize(int.Parse(rowPerPage.ElementAt(0)));
+            if (rowPerPagePossibilities.Count > 0)
+                SetPageSize(int.Parse(rowPerPagePossibilities.ElementAt(0)));
         }
 
         public abstract void LoadData();
@@ -57,9 +69,29 @@ namespace KretaDesktop.ViewModel.BaseClass
         public void LastPage()
         {
             QueryString.CurrentPage = QueryString.NumberOfPage;
+            OnPropertyChanged(nameof(CurrentPage));
             LoadData();
         }
 
-    
+        public void FirstPage()
+        {
+            QueryString.CurrentPage = 1;
+            OnPropertyChanged(nameof(CurrentPage));
+            LoadData();
+        }
+
+        public void PreviusPage()
+        {
+            QueryString.CurrentPage -= 1;
+            OnPropertyChanged(nameof(CurrentPage));
+            LoadData();
+        }
+
+        public void NextPage()
+        {
+            QueryString.CurrentPage += 1;
+            OnPropertyChanged(nameof(CurrentPage));
+            LoadData();
+        }  
     }
 }
