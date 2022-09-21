53e5489c85fa781f9ac425dc45f80feaa38260ac
Csaba Gyuris <gyuris.csaba@vasvari.org>
2022-09-21 13:15:19 +0200

    Testbe controller inicizalizálás

---
+++ KretaWebApiTest/Controllers/MappingDataTest.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KretaWebApiTest.Controllers
+{
+    public class MappingDataTest
+    {
+        public Mock<IMapper> MappingData()
+        {
+            var mappingService = new Mock<IMapper>();
+
+            /*
+            UserDetail im = getUserDetail(); // get value of UserDetails
+
+            mappingService.Setup(m => m.Map<UserDetail, UserDetailViewModel>(It.IsAny<UserDetail>())).Returns(interview); // mapping data
+            mappingService.Setup(m => m.Map<UserDetailViewModel, UserDetail>(It.IsAny<UserDetailtViewModel>())).Returns(im); // mapping data
+            */
+            return mappingService;
+        }
+    }
+}
--- KretaWebApiTest/Controllers/SubjectsControllerTests.cs
+++ KretaWebApiTest/Controllers/SubjectsControllerTests.cs
@@ -1,6 +1,10 @@
-﻿using Kreta.Repositories;
+﻿using AutoMapper;
+using Kreta.Models.Context;
+using Kreta.Repositories;
+using Kreta.Repositories.Interfaces;
 using KretaWebApi.Controllers;
 using Moq;
+using ServiceKretaLogger;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,6 +17,21 @@ namespace KretaWebApiTest.Controllers
     {
         private readonly Mock<SubjectRepo> mock;
         private readonly SubjectController controller;
+        private ILoggerManager logger;
+        private IRepositoryWrapper repositoryWrapper;
+        private MappingDataTest mappingData;
 
+        public SubjectsControllerTests()
+        {
+            KretaContext context = new KretaContext();
+
+            logger=new LoggerManager();
+            repositoryWrapper = new RepositoryWrapper(context);
+            mappingData= new MappingDataTest();
+
+            mock = new Mock<SubjectRepo>();
+            controller = new SubjectController(logger,repositoryWrapper, mappingData.MappingData().Object);
+            
+        }
     }
 }
