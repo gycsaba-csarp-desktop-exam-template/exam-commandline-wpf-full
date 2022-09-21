a6da0a9cf3fc67f5083ba700d7b97ac27dec7e19
Csaba Gyuris <gyuris.csaba@vasvari.org>
2022-09-21 12:21:08 +0200

    Projekt, mock és controller

---
+++ KretaWebApiTest/Controllers/SubjectsControllerTests.cs
@@ -0,0 +1,18 @@
+﻿using Kreta.Repositories;
+using KretaWebApi.Controllers;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KretaWebApiTest.Controllers
+{
+    public class SubjectsControllerTests
+    {
+        private readonly Mock<SubjectRepo> mock;
+        private readonly SubjectController controller;
+
+    }
+}
