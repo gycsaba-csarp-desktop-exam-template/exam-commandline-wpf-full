80eb9cb47742160f24b4d36ac7c501d6cca37669
Csaba Gyuris <gyuris.csaba@vasvari.org>
2022-09-27 10:24:34 +0200

    ExceptionMiddleware

---
+++ KretaWebApi/ExceptionHandler/ExceptionMiddleware.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net;
+
+namespace KretaWebApi.ExceptionHandler
+{
+    public static class ExceptionMiddleware
+    {
+        public static void ConfigureExceptionHandler(this IApplicationBuilder app, bool isDev)
+        {
+            app.UseExceptionHandler(appError =>
+            {
+                appError.Run(async context =>
+                {
+                    context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                    context.Response.ContentType = "application/json";
+
+                    var contextFeature = context.Features.Get<IExceptionHandlerPathFeature>();
+                    if (contextFeature != null)
+                    {
+                        var ex = contextFeature?.Error;
+                        await context.Response.WriteAsync(JsonConvert.SerializeObject(
+                            new ProblemDetails
+                            {
+
+                                Type = ex.GetType().Name,
+                                Status = (int)HttpStatusCode.InternalServerError,
+                                Instance = contextFeature?.Path,
+                                Title = isDev ? $"{ex.Message}" : "An error occurred.",
+                                Detail = isDev ? ex.StackTrace : null
+                            }));
+                    }
+                });
+            });
+        }
+
+    }
+}
