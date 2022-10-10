091aa69867c2612318faf496bd246171c46d6119
Csaba Gyuris <gyuris.csaba@vasvari.org>
2022-10-10 13:24:49 +0200

    Minden About menüpont működik

---
+++ KretaRazorPages/Pages/Menu/AboutMe/CSarpBackend.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace KretaRazorPages.Pages.Menu.AboutMe
+{
+    public class CSarpBackendModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ KretaRazorPages/Pages/Menu/AboutMe/SpringBackend.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace KretaRazorPages.Pages.Menu.AboutMe
+{
+    public class SpringBackendModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
