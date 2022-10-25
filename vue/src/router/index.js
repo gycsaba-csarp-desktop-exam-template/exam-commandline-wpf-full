f23f75e82b06284692caf66837fefca8cf7a3428
Csaba Gyuris <gyuris.csaba@vasvari.org>
2022-10-25 13:25:01 +0200

    Home componens működik

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
-import component from ' @/component/Home'
+import Home from '@/components/Home'
 
 Vue.use(Router)
 
